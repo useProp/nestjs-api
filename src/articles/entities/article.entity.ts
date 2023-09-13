fb04baac3488ef14afff8dcab3cf35837cf7a47e
useprop <krokzfine@gmail.com>
2023-09-13 11:45:41 +0300

    base

---
+++ src/articles/entities/article.entity.ts
@@ -0,0 +1,24 @@
+import { Article } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ArticleEntity implements Article {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  description: string | null;
+
+  @ApiProperty()
+  body: string;
+
+  published: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
